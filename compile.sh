e9a3f964b52e742d5c65e615c9d6e9cca7f989c8
TaWeiTu <tu.da.wei@gmail.com>
2020-06-20 16:46:12 +0800

    Add remaining sample codes

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ./ksocket
+make
+insmod ksocket.ko
+cd ../master_device
+make
+insmod master_device.ko
+cd ../slave_device
+make
+insmod slave_device.ko
+cd ../user_program
+make
+
+
