ca0d8511a1317ac888f5eae1c0e72c3aac7c550b
TaWeiTu <tu.da.wei@gmail.com>
2020-06-22 00:46:35 +0800

    Format

--- master_device/master_device.c
+++ master_device/master_device.c
@@ -35,7 +35,7 @@
 
 typedef struct socket *ksocket_t;
 
-struct dentry *file1; // debug file
+struct dentry *file1;  // debug file
 
 // functions about kscoket are exported, and thus we use extern here
 extern ksocket_t ksocket(int domain, int type, int protocol);
@@ -46,8 +46,8 @@ extern ksocket_t kaccept(ksocket_t socket, struct sockaddr *address,
 extern ssize_t ksend(ksocket_t socket, const void *buffer, size_t length,
                      int flags);
 extern int kclose(ksocket_t socket);
-extern char *
-inet_ntoa(struct in_addr *in); // DO NOT forget to kfree the return pointer
+extern char *inet_ntoa(
+    struct in_addr *in);  // DO NOT forget to kfree the return pointer
 
 static int __init master_init(void);
 static void __exit master_exit(void);
@@ -56,16 +56,16 @@ int master_close(struct inode *inode, struct file *filp);
 int master_open(struct inode *inode, struct file *filp);
 static long master_ioctl(struct file *file, unsigned int ioctl_num,
                          unsigned long ioctl_param);
-static ssize_t
-send_msg(struct file *file, const char __user *buf, size_t count,
-         loff_t *data); // use when user is writing to this device
+static ssize_t send_msg(
+    struct file *file, const char __user *buf, size_t count,
+    loff_t *data);  // use when user is writing to this device
 static int master_mmap(struct file *filp, struct vm_area_struct *vma);
 static void send_mmap(size_t count);
 
 static ksocket_t sockfd_srv,
-    sockfd_cli;                     // socket for master and socket for slave
-static struct sockaddr_in addr_srv; // address for master
-static struct sockaddr_in addr_cli; // address for slave
+    sockfd_cli;                      // socket for master and socket for slave
+static struct sockaddr_in addr_srv;  // address for master
+static struct sockaddr_in addr_cli;  // address for slave
 static mm_segment_t old_fs;
 static int addr_len;
 // static  struct mmap_info *mmap_msg; // pointer to the mapped data in this
@@ -79,8 +79,8 @@ static struct file_operations master_fops = {.owner = THIS_MODULE,
                                              .unlocked_ioctl = master_ioctl,
                                              .open = master_open,
                                              .write = send_msg,
-                                             .release = master_close, 
-																						 .mmap = master_mmap};
+                                             .release = master_close,
+                                             .mmap = master_mmap};
 
 // device info
 static struct miscdevice master_dev = {
@@ -162,40 +162,41 @@ static long master_ioctl(struct file *file, unsigned int ioctl_num,
   old_fs = get_fs();
   set_fs(KERNEL_DS);
   switch (ioctl_num) {
-  case master_IOCTL_CREATESOCK: // create socket and accept a connection
-    sockfd_cli = kaccept(sockfd_srv, (struct sockaddr *)&addr_cli, &addr_len);
-    if (sockfd_cli == NULL) {
-      printk("accept failed\n");
-      return -1;
-    } else
-      printk("aceept sockfd_cli = 0x%p\n", sockfd_cli);
+    case master_IOCTL_CREATESOCK:  // create socket and accept a connection
+      sockfd_cli = kaccept(sockfd_srv, (struct sockaddr *)&addr_cli, &addr_len);
+      if (sockfd_cli == NULL) {
+        printk("accept failed\n");
+        return -1;
+      } else {
+        printk("aceept sockfd_cli = 0x%p\n", sockfd_cli);
+      }
 
-    tmp = inet_ntoa(&addr_cli.sin_addr);
-    printk("got connected from : %s %d\n", tmp, ntohs(addr_cli.sin_port));
-    kfree(tmp);
-    ret = 0;
-    break;
-  case master_IOCTL_MMAP:
-    send_mmap(ioctl_param);
-    ret = 0;
-    break;
-  case master_IOCTL_EXIT:
-    if (kclose(sockfd_cli) == -1) {
-      printk("kclose cli error\n");
-      return -1;
-    }
-    ret = 0;
-    break;
-  default:
-    pgd = pgd_offset(current->mm, ioctl_param);
-    p4d = p4d_offset(pgd, ioctl_param);
-    pud = pud_offset(p4d, ioctl_param);
-    pmd = pmd_offset(pud, ioctl_param);
-    ptep = pte_offset_kernel(pmd, ioctl_param);
-    pte = *ptep;
-    printk("master: %lX\n", pte);
-    ret = 0;
-    break;
+      tmp = inet_ntoa(&addr_cli.sin_addr);
+      printk("got connected from : %s %d\n", tmp, ntohs(addr_cli.sin_port));
+      kfree(tmp);
+      ret = 0;
+      break;
+    case master_IOCTL_MMAP:
+      send_mmap(ioctl_param);
+      ret = 0;
+      break;
+    case master_IOCTL_EXIT:
+      if (kclose(sockfd_cli) == -1) {
+        printk("kclose cli error\n");
+        return -1;
+      }
+      ret = 0;
+      break;
+    default:
+      pgd = pgd_offset(current->mm, ioctl_param);
+      p4d = p4d_offset(pgd, ioctl_param);
+      pud = pud_offset(p4d, ioctl_param);
+      pmd = pmd_offset(pud, ioctl_param);
+      ptep = pte_offset_kernel(pmd, ioctl_param);
+      pte = *ptep;
+      printk("master: %lX\n", pte);
+      ret = 0;
+      break;
   }
 
   set_fs(old_fs);
@@ -205,8 +206,7 @@ static ssize_t send_msg(struct file *file, const char __user *buf, size_t count,
                         loff_t *data) {
   // call when user is writing to this device
   char msg[BUF_SIZE];
-  if (copy_from_user(msg, buf, count))
-    return -ENOMEM;
+  if (copy_from_user(msg, buf, count)) return -ENOMEM;
   ksend(sockfd_cli, msg, count, 0);
 
   return count;
